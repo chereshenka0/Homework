2682a43c18ca0cc6dd14b25134c71733e3a80a85
chereshenka0 <liss030780@gmail.com>
2024-09-18 19:13:29 -0400

    2 numbers

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //номер 1
+        int x = 10;
+        System.out.println(x);
+        double d = 3.14;
+        System.out.println("d = "+ d);
+        boolean o = true;
+        System.out.println(o);
+        char c = 'A';
+        System.out.println(c);
+        String st = "Hello World!";
+
+        //номер 2
+        int j = 5;
+        System.out.println(j);
+        final double p = 3.14;
+        System.out.println(p);
+        String sc = "John";
+        int age = 25;
+        System.out.println(age);
+        int f = 100;
+        System.out.println(f);
+        System.out.println(sc +" "+ st);
+
+
+
+
+
+        }
+    }
