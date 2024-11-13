1d938f4856851470dfd984d36e0a955f458d4e32
chereshenka0 <liss030780@gmail.com>
2024-11-13 10:47:54 -0500

    delete empty rows

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         //номер 1
-        Random random = new Random();
+        /*Random random = new Random();
         int number = random.nextInt(100) +1;
 
         Scanner input = new Scanner(System.in);
@@ -34,3 +34,115 @@ public class Main {
         }
         }
     }
+
+//задача 2
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Enter positive integers to calculate the sum of even numbers.");
+        System.out.println("The program will end when you enter 0");
+
+        while (true) {
+            System.out.println("Enter a number: ");
+            int number = input.nextInt();
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number % 2 == 0) {
+                sum += number;
+            }
+            System.out.println("The sum of all even numbers entered: " + sum);
+        }
+
+
+        //задача 3
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer to check if it is prime.");
+        System.out.println("The program will end when you enter 0");
+
+        while(true){
+            System.out.println("Enter your number: ");
+            int number = input.nextInt();
+
+            if(number == 0){
+                System.out.println("The program exits");
+                break;
+            }
+
+            if(number){//Не очень поняла что писать в if, чтобы он выводил простое число
+                System.out.println(number + " is a prime number.");
+                else{
+                    System.out.println(number + " is not a prime number.");
+                }
+            }
+        }
+
+        //задача 4
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the calculator!");
+        System.out.println("You can perform addition, subtraction, multiplication and division operations.");
+
+
+        while (true) {
+            System.out.println("\nChoose an operation:");
+            System.out.println("1 - addition");
+            System.out.println("2 - subtraction");
+            System.out.println("3 - multiplication");
+            System.out.println("4 - division");
+            System.out.println("0 - exit");
+            System.out.print("Type a number of the operation: ");
+
+            int operation = input.nextInt();
+
+            if (operation == 0) {
+                System.out.println("Exit from the calculator.");
+                break;
+            }
+
+            System.out.print("Type the first number: ");
+            double num1 = input.nextDouble();
+            System.out.print("Введите второе число: ");
+            double num2 = input.nextDouble();
+
+            double result = 0;
+
+            switch (operation) {
+                case 1:
+                    result = num1 + num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case 4:
+                    if (num2 == 0) {
+                        System.out.println("Error. You can't divide by 0.");
+                    } else {
+                        result = num1 / num2;
+                        System.out.println("Result: " + result);
+                    }
+                    break;
+                default:
+                    System.out.println("Incorrect operation selection.");
+            }
+        }*/
+        //Прочитала про switch в gpt и попробовала заюзать в задании(на уроке хотела бы еще спросить про switch)
+
+
+        //задача 5
+        //Не поняла как сделать задание, смотрела в инете, все равно не особо понятно было
+
+
+
+
+    }
+
+}
+
+
