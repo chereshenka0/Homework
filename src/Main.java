321a75ba7b042cb3e71d2d2cdd1d54f483e378d5
chereshenka0 <liss030780@gmail.com>
2024-11-13 09:49:23 -0500

    delete empty rows

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         //номер 1
-        int x = 10;
-        System.out.println(x);
-        double d = 3.14;
-        System.out.println("d = "+ d);
-        boolean o = true;
-        System.out.println(o);
-        char c = 'A';
-        System.out.println(c);
-        String st = "Hello World!";
+        Random random = new Random();
+        int number = random.nextInt(100) +1;
 
-        //номер 2
-        int j = 5;
-        System.out.println(j);
-        final double p = 3.14;
-        System.out.println(p);
-        String sc = "John";
-        int age = 25;
-        System.out.println(age);
-        int f = 100;
-        System.out.println(f);
-        System.out.println(sc +" "+ st);
+        Scanner input = new Scanner(System.in);
+        int userGuess = -1;
+        System.out.println("Try to guess the number that i set up for you!");
+        System.out.println("If you want to exit, type 0");
 
+        while(userGuess != number){
+            System.out.print("Type the number: ");
+            userGuess = input.nextInt();
+
+            if(userGuess == 0){
+                System.out.println("You are exit the game");
+                break;
+            }
+
+            if(userGuess < number){
+                System.out.println("The number is greater. Try again");
+            }
+            else if(userGuess > number){
+                System.out.println("The number is less. Try again");
+            }
+
+        }
+        if(userGuess == number){
+            System.out.println("Congratulations, you guessed the number " + number + "!");
+        }
         }
     }
